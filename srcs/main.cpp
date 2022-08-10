7ced982efd58440e2eebc00bfa71b2062636fa4a
Matthieu Servagent <mservage@z3r10p3.42lyon.fr>
2022-08-10 15:53:19 +0200

    gestion d'erreur de port dans main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matthieu <matthieu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mservage <mservage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 13:16:19 by mservage          #+#    #+#             */
-/*   Updated: 2022/07/04 15:39:25 by matthieu         ###   ########.fr       */
+/*   Updated: 2022/08/10 15:09:15 by mservage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,18 @@ int	main(int ac, char **av)
 		std::cout << "Wrongs arguments" << std::endl;
 		return (1);
 	}
-
+	int incr = 0;
+	std::string arg = av[1];
+	while (av[1][incr])
+	{
+		if (av[1][incr] >= '0' && av[1][incr] <= '9')
+			incr++;
+		else
+		{
+			std::cout << "Wrongs port" << std::endl;
+			return (1);
+		}
+	}
 	Server		server("ServEau", atoi(av[1]), av[2]);
 	server.init_listen();
 	server.wait_for_event();
